c5d94c127d9ea7b8fb573117a21d4c874d0ef8cc
carlozanieri <carlo.zanieri@gmail.com>
2025-04-05 01:30:50 +0200

    Versione 2.4

--- Connect.py
+++ Connect.py
@@ -279,7 +279,7 @@ class Connect:
         url = 'http://carlozanieri.it/home/carlo/IncantoPipe'
         response = requests.get(url, stream=True)
 
-        with open('lista_partecipants.csv', 'wb') as out_file:
+        with open('lista_partecipantis.csv', 'wb') as out_file:
             shutil.copyfileobj(response.raw, out_file)
 
             print('The file was saved successfully')
